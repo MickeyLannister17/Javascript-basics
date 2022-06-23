af919f31d513dd2abdc721a235bbef59cc5521b2
Senaike Michael <senaikepooldis@gmail.com>
2022-06-23 15:32:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const details = {
+//   name: "Lannister",
+//   height: "6.5",
+//   nationality: "Nigeria",
+// };
+
+// console.log(details);
+
+console.log("\nName: Lannister".toUpperCase());
+console.log("\nheight: 6.2".toUpperCase());
+console.log("\nnationality: nigeria".toUpperCase());
